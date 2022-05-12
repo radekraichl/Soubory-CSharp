481a69d729dedcdad5ba2b25e56fa9f18dee6dce
Radek Raichl <radekraichl@gmail.com>
2022-05-12 21:06:16 +0200

    Update project Svatky

--- Svatky/Program.cs
+++ Svatky/Program.cs
@@ -2,31 +2,30 @@
 using System.IO;
 using System.Linq;
 
-namespace Svatky
+namespace Svatky;
+
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main()
+        Console.WriteLine($"Dnes je {DateTime.Today.ToLongDateString()}");
+        Console.WriteLine($"Svátek má {VratSvatek(DateTime.Today.Day, DateTime.Today.Month)}");
+        Console.WriteLine($"Zítra má svátek {VratSvatek(DateTime.Today.Day + 1, DateTime.Today.Month)}");
+    }
+
+    static string VratSvatek(int den, int mesic)
+    {
+        string[] jmena = File.ReadAllLines(@"..\..\..\jmena.txt");
+
+        // odstraní ze začátku pole jmena[] nepotřebné měsíce
+        for (int i = 0; i < mesic - 1; i++)
         {
-            Console.WriteLine($"Dnes je {DateTime.Today.ToLongDateString()}");
-            Console.WriteLine($"Svátek má {VratSvatek(DateTime.Today.Day, DateTime.Today.Month)}");
-            Console.ReadLine();
+            jmena = jmena.SkipWhile(j => j != string.Empty).Skip(1).ToArray();
         }
 
-        static string VratSvatek(int den, int mesic)
-        {
-            string[] jmena = File.ReadAllLines(@"..\..\..\jmena.txt");
+        // odstraní prázdných řádků z pole jmena[] (kvůli přetečení do dalšího měsíce)
+        jmena = jmena.Where(x => x != string.Empty).ToArray();
 
-            // odstraní ze začátku pole jmena[] nepotřebné měsíce
-            for (int i = 0; i < mesic - 1; i++)
-            {
-                jmena = jmena.SkipWhile(j => j != string.Empty).Skip(1).ToArray();
-            }
-
-            // odstraní z pole jmena[] zbytek měsíců
-            jmena = jmena.TakeWhile(x => x != string.Empty).ToArray();
-
-            return jmena.ElementAt(den - 1);
-        }
+        return jmena.ElementAt(den - 1);
     }
 }
